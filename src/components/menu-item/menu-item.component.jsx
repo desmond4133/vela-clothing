5bab1940c070ad111e2f721535cc2af034eed9b6
duna2 <36515780+duna2@users.noreply.github.com>
2021-04-09 00:18:29 +0100

    increased size of two menu-items

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl}) => (
+const MenuItem = ({title, imageUrl, size}) => (
 
-    <div  className='menu-item'>
+    <div  className={`${size} menu-item`}>
      
      <div className='background-image' style = {{
         backgroundImage: `url(${imageUrl})`
