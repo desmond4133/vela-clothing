b31c2c57d85b886bb10fd74f94095bbbef4890a8
duna2 <36515780+duna2@users.noreply.github.com>
2021-04-16 14:52:15 +0100

    added new fonts to app.css and fiddled with menu-item syles

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -8,14 +8,11 @@ const MenuItem = ({title, imageUrl, size}) => (
      <div className='background-image' style = {{
         backgroundImage: `url(${imageUrl})`
      }} >
-
      </div>
 
      <div className='content'>
-
         <h1 className='title'>{title}</h1>
         <span className='subtitle'>SHOP NOW</span>
-
      </div>
 
     </div>
