257d600a106580621cf3b5c4659d0cc8fe490cb5
duna2 <36515780+duna2@users.noreply.github.com>
2021-04-17 13:08:12 +0100

    added collection-preview component and syling, by passing in props from ShopPage state data

---
+++ src/components/collection-preview/collection-preview.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './collection-preview.styles.scss';
+
+// destructured the props of the CollectionPreview from ShopPage and mapped the props to be displayed
+
+const CollectionPreview = ({ title, items }) => (
+    
+    <div className='collection-preview'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <div>
+            {items.map((item) => (
+                <div key={item.id}>{item.name}</div>
+                ))}
+        </div>
+    </div>
+);
+
+export default CollectionPreview;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,17 +2,28 @@ import React from 'react';
 
 import SHOP_DATA from './shop.data.js';
 
+import CollectionPreview from '../../components/collection-preview/collection-preview.jsx'
+
 
 class ShopPage extends React.Component {
     constructor() {
         super()
-        this.state = { collections: SHOP_DATA }
+        this.state = { collections: SHOP_DATA };
     }
 
 
 
     render() {
-        return <div>SHOP PAGE</div>
+        const { collections } = this.state;
+        
+        return (
+        <div>
+        {collections.map(({id, ...otherCollectionProps}) => (
+            <CollectionPreview key={id} { ...otherCollectionProps } />
+        
+        ))}
+        </div>
+        )
     }
 }
 
