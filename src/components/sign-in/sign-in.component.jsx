5c5c08677724968930df8f6ffee92f301dd2ca4d
duna2 <36515780+duna2@users.noreply.github.com>
2021-04-27 20:28:41 +0100

    created forminput compononent with styling

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import './sign-in.styles.sass'
-
+import './sign-in.styles.sass';
+import FormInput from '../../form-input.component.jsx'
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -11,7 +11,6 @@ class SignIn extends React.Component {
             password: ''
         }
 
-        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
 
@@ -21,18 +20,36 @@ class SignIn extends React.Component {
         this.setState({email: '', password: ''})
     }
 
+    handleChange= event => {
+        const { name, value } = event.target;
+
+        this.setState({ [name]: value});
+    };
+
     render() {
         return (
-            <div>
-                <hi>I already have an account</hi>
+            <div className='sign-in'>
+                <h1>I already have an account</h1>
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input name='email' type='email' value={this.state.email} required/>
-                    <label>Email</label>
-                    <input name='password' type='password' value={this.state.email} required/>
-                    <label>Password</label>
-
+                    <FormInput 
+                    name='email' 
+                    type='email' 
+                    value={this.state.email}
+                    handleChange={this.handleChange}
+                    required
+                    label='email'
+                    />
+        
+                    <FormInput 
+                    name='password' 
+                    type='password' 
+                    value={this.state.email} 
+                    handleChange={this.handleChange}
+                    required
+                    label='password'
+                    />
                     <input type='submit' value='Submit Form'/>
                 </form>
             </div>
@@ -40,3 +57,5 @@ class SignIn extends React.Component {
     }
 }
 
+
+export default SignIn;
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 import './sign-in-sign-up.styles.sass';
+import SignIn from '../../components/sign-in/sign-in.component';
 
 
 const signInAndSignUpPage = () => {
 
-    return <div className='sign-in-sign-up'>SIGN IN</div>
+    return <div className='sign-in-sign-up'>
+        <SignIn/>
+    </div>
     
 };
 
